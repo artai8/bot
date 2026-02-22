87c515a1d9f407c1f34ddf43159ff4345dd30fc7
artai8 <109020988+artai8@users.noreply.github.com>
2026-02-22 13:48:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import sys
 from datetime import datetime
 
 from config import (
-    API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS,
+    API_HASH, API_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS,
     CHANNEL_ID, PORT
 )
 import config as cfg
@@ -29,7 +29,7 @@ class Bot(Client):
         super().__init__(
             name="Bot",
             api_hash=API_HASH,
-            api_id=APP_ID,
+            api_id=API_ID,
             plugins={"root": "plugins"},
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN,
